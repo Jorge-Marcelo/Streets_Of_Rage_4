be4f6f218e1a23b33d33b938c01e5c651e5711b7
Jorge-Marcelo <jorge3258@hotmail.com>
2023-09-04 01:20:51 -0300

    Aplicando correções no slider de Personagens

--- js/slidesModal.js
+++ js/slidesModal.js
@@ -1,33 +1,28 @@
-// Variável para rastrear o índice do slide atual
+
 let inidicesModalSlides = 1;
 
-// Inicializa o slider com o primeiro slide
 mostrarModalSlides(inidicesModalSlides);
 
-// Função para controlar o slider
 function SliderControl(indice) {
-  mostrarModalSlides(inidicesModalSlides = inidicesModalSlides + indice); // Use a variável inidicesModalSlides
+mostrarModalSlides(inidicesModalSlides = inidicesModalSlides + indice);
 }
 
 
-// Função para mostrar um slide específico
 function mostrarModalSlides(indice) {
-  let contadorModalSlides;
-  let slidesModal = document.getElementsByClassName("slidesModal");
+let contadorModalSlides;
+let slidesModal = document.getElementsByClassName("slidesModal");
 
-  // Verifica se o número do slide está fora dos limites
-  if (indice > slidesModal.length) {
-    inidicesModalSlides = 1;
-  }
-  if (indice < 1) {
-    inidicesModalSlides = slidesModal.length;
-  }
+if (indice > slidesModal.length) {
+inidicesModalSlides = 1;
+}
 
-  // Oculta todos os slides
-  for (contadorModalSlides = 0; contadorModalSlides < slidesModal.length; contadorModalSlides++) {
-    slidesModal[contadorModalSlides].classList.remove("active");
-  }
+if (indice < 1) {
+inidicesModalSlides = slidesModal.length;
+}
 
-  // Mostra apenas o slide ativo
-  slidesModal[inidicesModalSlides - 1].classList.add("active");
+
+for (contadorModalSlides = 0; contadorModalSlides < slidesModal.length; contadorModalSlides++){
+slidesModal[contadorModalSlides].classList.remove("active");
+}
+slidesModal[inidicesModalSlides - 1].classList.add("active");
 }
